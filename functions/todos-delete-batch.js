05a47543532f4c2ab36c3716536502440b8c132f
Clay Stewart <cstewart@rosettastone.com>
2019-12-26 13:16:02 -0600

    mostly prettier

--- functions/todos-delete-batch.js
+++ functions/todos-delete-batch.js
@@ -1,32 +1,34 @@
 /* Import faunaDB sdk */
-const faunadb = require('faunadb')
+const faunadb = require("faunadb");
 
-const q = faunadb.query
+const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
-})
+});
 
 exports.handler = async (event, context) => {
-  const data = JSON.parse(event.body)
-  console.log('data', data)
-  console.log('Function `todo-delete-batch` invoked', data.ids)
+  const data = JSON.parse(event.body);
+  console.log("data", data);
+  console.log("Function `todo-delete-batch` invoked", data.ids);
   // construct batch query from IDs
-  const deleteAllCompletedTodoQuery = data.ids.map((id) => {
-    return q.Delete(q.Ref(`classes/todos/${id}`))
-  })
+  const deleteAllCompletedTodoQuery = data.ids.map(id => {
+    return q.Delete(q.Ref(`classes/todos/${id}`));
+  });
   // Hit fauna with the query to delete the completed items
-  return client.query(deleteAllCompletedTodoQuery)
-    .then((response) => {
-      console.log('success', response)
+  return client
+    .query(deleteAllCompletedTodoQuery)
+    .then(response => {
+      console.log("success", response);
       return {
         statusCode: 200,
         body: JSON.stringify(response)
-      }
-    }).catch((error) => {
-      console.log('error', error)
+      };
+    })
+    .catch(error => {
+      console.log("error", error);
       return {
         statusCode: 400,
         body: JSON.stringify(error)
-      }
-    })
-}
+      };
+    });
+};
